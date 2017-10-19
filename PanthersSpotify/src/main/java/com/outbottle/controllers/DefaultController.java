cf64ad66d044c47088235ad9b430b81723ba48ef
Weichao Zhao <weichao.zhao@stonybrook.edu>
2017-10-19 05:22:49 -0400

    set up with maven spring mvc

---
+++ PanthersSpotify/src/main/java/com/outbottle/controllers/DefaultController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.outbottle.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author john
+ */
+@Controller
+public class DefaultController {
+   
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String index(ModelMap map) {
+       map.put("msg", "Hello Springsdsdsas 4 Web MVC!");
+       return "index";
+   }
+    
+}

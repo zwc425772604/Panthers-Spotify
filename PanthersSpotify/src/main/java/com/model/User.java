c092f40fde681fe2c6d151d83f222b0e9a740ed2
Weichao Zhao <weichao.zhao@stonybrook.edu>
2017-10-20 14:06:27 -0400

    add jpa entity user class, and database connection

---
+++ PanthersSpotify/src/main/java/com/model/User.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Weichao ZHao
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    
+    public User()
+    {
+        
+    }
+    public User(int id, String username, String email, String password)
+    {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.password = password;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    public String getUsername()
+    {
+        return username;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.model.User[ id=" + id + " ]";
+    }
+    
+}
